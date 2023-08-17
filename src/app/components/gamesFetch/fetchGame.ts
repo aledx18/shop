8e01e8c2f881f36c1550f02a2a9053dd085e6f25
aledx18 <alediiaz18@gmail.com>
2023-08-17 20:21:38 -0300

    deploy

--- src/app/components/gamesFetch/fetchGame.ts
+++ src/app/components/gamesFetch/fetchGame.ts
@@ -1,12 +1,15 @@
 export async function getGames({ page }: { page: number }) {
-  const res = await fetch(`http://localhost:3000/api/game?page=${page}`, {
-    cache: 'no-store'
-  })
+  const res = await fetch(
+    `http://gameshop-pearl.vercel.app/api/game?page=${page}`,
+    {
+      cache: 'no-store'
+    }
+  )
   const data = await res.json()
   return data
 }
 
 export async function getGameById(id: string) {
-  const res = await fetch(`http://localhost:3000/api/game/${id}`)
+  const res = await fetch(`http://gameshop-pearl.vercel.app/api/game/${id}`)
   return await res.json()
 }
